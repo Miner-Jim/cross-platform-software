5787d1b319f2889b8e1f65ad614b8eb342f896de
MShmel <dimkabox@yandex.ru>
2025-10-03 22:00:10 +0300

    изменен импорт RequestBody

--- src/main/java/com/example/demo/controller/DeviceController.java
+++ src/main/java/com/example/demo/controller/DeviceController.java
@@ -7,6 +7,7 @@ import com.example.demo.Service.DeviceService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
 
 @RestController
