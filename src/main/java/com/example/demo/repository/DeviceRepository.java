040e514eeacd0733730dfea898da000825e536c5
MShmel <dimkabox@yandex.ru>
2025-10-10 14:52:20 +0300

    дополнен JpaSpecificationExecutor

--- src/main/java/com/example/demo/repository/DeviceRepository.java
+++ src/main/java/com/example/demo/repository/DeviceRepository.java
@@ -2,14 +2,16 @@ package com.example.demo.repository;
 
 import java.util.List;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.Device;
 import com.example.demo.model.DeviceType;
 
 @Repository
-public interface DeviceRepository extends JpaRepository<Device, Long> {
+public interface DeviceRepository extends JpaRepository<Device, Long>, JpaSpecificationExecutor<Device>{
     // Найти все устройства по типу
     List<Device> findByType(DeviceType type);
     
